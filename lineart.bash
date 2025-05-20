f5d29a28b40c9c6d3df22807f5471d2cdcf3049f
David Beck <beck.dac@live.com>
2025-05-19 22:54:17 -0700

    easier to edit the dimensions of plot

--- lineart.bash
+++ lineart.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+dim=7cm
+
 echo line plotting
 vpype read lineart.svg \
-    scaleto 200px 200px \
+    scaleto $dim $dim \
     splitall \
     linemerge --tolerance 0.1mm \
     linesort \
     reloop \
     linesimplify \
     filter --min-length 0.5mm \
-    write --center --page-size 220x240 lineart.out.svg
+    write --center --page-size ${dim}x$dim lineart.out.svg
 
 echo gcode rendering
 vpype --config qtdraw.toml \
