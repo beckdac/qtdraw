236b1d9927ecbc8950553095f0b5ea3d5fffdb01
David Beck <beck.dac@live.com>
2025-05-24 00:39:02 -0700

    working mesh to gcode

--- mesh/qtdraw_mesh.py
+++ mesh/qtdraw_mesh.py
@@ -4,6 +4,7 @@ import click
 import numpy as np
 import pandas as pd
 
+@click.command()
 @click.option('--lim', nargs=2, type=int, default=(400, 400), \
         help='Size limit of the work area in mm')
 @click.option('--div', nargs=2, type=int, default=(6, 6), \
@@ -16,21 +17,17 @@ import pandas as pd
         help='Max probe depth from 0')
 @click.option('--travel_height', type=int, default=2, \
         help='Travel height for Z from sample to sample')
-@click.option('--safe_height', type=int, default=0, \
+@click.option('--safe_height', type=int, default=15, \
         help='Safe height after meshing')
 @click.option('--output_filename', type=str, default='qtdraw_mesh.gcode', \
         help='Filename to use for the gcode output')
-def qt_mesh(lim: tuple[int,int]=[400,400], div: tuple[int, int]=(6,6), \
-        feed: int=500, seek: int=100, \
-        probe_depth: int=-1, travel_height: int=2, \
-        safe_height: int=25, \
-        output_filename: str='qtdraw_mesh.gcode'):
+def qt_mesh(lim, div, feed, seek, probe_depth, travel_height, safe_height, output_filename):
     """Generate G-code for fluidnc and parse the output to build
     a height map of the bed"""
     xv = np.linspace(0, lim[0], div[0])
     yv = np.linspace(0, lim[1], div[1])
 
-    print(f"generating {div[0] * div[1]} points on a grid from ({xv[0]},{yv[0]}) to ({xv[1]},{yv[1]})")
+    print(f"generating {div[0] * div[1]} points on a grid from ({xv[0]},{yv[0]}) to ({xv[-1]},{yv[-1]})")
 
     with(open(output_filename, 'w') as file):
         # preamble
@@ -42,24 +39,25 @@ def qt_mesh(lim: tuple[int,int]=[400,400], div: tuple[int, int]=(6,6), \
         # go to the middle to use as a reference
         file.write(f"G1 X{lim[0]/2} Y{lim[1]/2} F{feed}\n")
         # go to search start
-        file.write(f"G0 Z{travel_height}\n")
+        file.write(f"G0 Z{travel_height} F{seek}\n")
         # probe
         file.write(f"G38.2 Z{probe_depth} F{seek}\n")
         # set the work 0
         file.write(f"G92 Z0\n")
         # go to travel_height
-        file.write(f"G0 Z{travel_height}\n")
+        file.write(f"G0 Z{travel_height} F{seek}\n")
         # probe at a slower speed
         file.write(f"G38.2 Z{probe_depth} F{fine_seek}\n")
         file.write(f"G92 Z0\n")
-        file.write(f"G0 Z{travel_height}\n")
+        file.write(f"G0 Z{travel_height} F{seek}\n")
     
         for x in xv:
             for y in yv:
                 file.write(f"G1 X{x} Y{y} F{feed}\n")
                 file.write(f"G38.2 Z{probe_depth} F{seek}\n")
-                file.write(f"G0 Z2\n")
-        file.write(f"G0 Z{safe_height}\n")
+                file.write(f"G0 Z2 F{seek}\n")
+
+        file.write(f"G0 Z{safe_height} F{seek}\n")
 
 
 if __name__ == '__main__':
