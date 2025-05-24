fb0c57c3521949a160b0c2934765707bfce64420
David Beck <beck.dac@live.com>
2025-05-24 01:23:17 -0700

    working baseline mesh generation

--- mesh/qtdraw_mesh.py
+++ mesh/qtdraw_mesh.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python
 
+import sys
+import queue
+import threading
+
 import click
 import numpy as np
 import pandas as pd
+import websocket
+
+# websocket portion
+def receiver(ws: websocket.WebSocket, q: queue.Queue, qdone: queue.Queue):
+    """Receive websocket messages and swallow pings and other session info
+    placing the input lines in a queue for the main thread."""
+    while True:
+        for line in ws.recv().splitlines():
+            if not isinstance(line, str):
+                line = str(line, 'utf-8')
+            if line.startswith("PING:") or \
+                  line.startswith("CURRENT_ID:") or \
+                  line.startswith("ACTIVE_ID:") or \
+                  line.startswith("<Run|Mpos"):
+                continue
+            else:
+                #print(line)
+                q.put(line)
+            if not qdone.empty():
+                return
+
+def ws_send_and_get(ws: websocket.WebSocket, q: queue.Queue, send: str, data: queue.Queue, expect: str="ok") -> str:
+    ws.send(send);
+    while True:
+        resp = q.get()
+        if resp.startswith(expect):
+            #print(f"expected response to {send} : {resp}")
+            break
+        print(f"unexpected response to {send} : {resp}")
+        data.put(resp)
+    return resp
 
 @click.command()
 @click.option('--lim', nargs=2, type=int, default=(400, 400), \
@@ -19,46 +54,67 @@ import pandas as pd
         help='Travel height for Z from sample to sample')
 @click.option('--safe_height', type=int, default=15, \
         help='Safe height after meshing')
-@click.option('--output_filename', type=str, default='qtdraw_mesh.gcode', \
-        help='Filename to use for the gcode output')
-def qt_mesh(lim, div, feed, seek, probe_depth, travel_height, safe_height, output_filename):
+@click.option('--uri', type=str, default='ws://qtdraw.local:81', \
+        help='Output websocket to use for gcode input and output')
+@click.option('--output_filename', type=str, default='qt_mesh.tsv')
+def qt_mesh(lim, div, \
+        feed, seek, probe_depth, travel_height, safe_height, \
+        output_filename, uri):
     """Generate G-code for fluidnc and parse the output to build
-    a height map of the bed"""
+    a height map of the bed by communicating over a websocket and
+    parsing the responses for probe messages."""
+
+    ws = websocket.WebSocket()
+    ws.connect(uri)
+    q = queue.Queue()
+    qdone = queue.Queue()
+    data = queue.Queue()
+    t = threading.Thread(target=receiver, args=(ws,q,qdone))
+    t.start()
+
     xv = np.linspace(0, lim[0], div[0])
     yv = np.linspace(0, lim[1], div[1])
 
     print(f"generating {div[0] * div[1]} points on a grid from ({xv[0]},{yv[0]}) to ({xv[-1]},{yv[-1]})")
 
-    with(open(output_filename, 'w') as file):
+    if True:
         # preamble
-        file.write(f"G90 G21 G17\n")
+        ws_send_and_get(ws, q, f"G90 G21 G17\n", data)
         fine_seek = seek / 2
         # fine touch off routine
         # travel to safe height
-        file.write(f"G0 Z{safe_height}\n")
+        ws_send_and_get(ws, q, f"G0 Z{safe_height}\n", data)
         # go to the middle to use as a reference
-        file.write(f"G1 X{lim[0]/2} Y{lim[1]/2} F{feed}\n")
+        ws_send_and_get(ws, q, f"G1 X{lim[0]/2} Y{lim[1]/2} F{feed}\n", data)
         # go to search start
-        file.write(f"G0 Z{travel_height} F{seek}\n")
+        ws_send_and_get(ws, q, f"G0 Z{travel_height} F{seek}\n", data)
         # probe
-        file.write(f"G38.2 Z{probe_depth} F{seek}\n")
+        ws_send_and_get(ws, q, f"G38.2 Z{probe_depth} F{seek}\n", data)
         # set the work 0
-        file.write(f"G92 Z0\n")
+        ws_send_and_get(ws, q, f"G92 Z0\n", data)
         # go to travel_height
-        file.write(f"G0 Z{travel_height} F{seek}\n")
+        ws_send_and_get(ws, q, f"G0 Z{travel_height} F{seek}\n", data)
         # probe at a slower speed
-        file.write(f"G38.2 Z{probe_depth} F{fine_seek}\n")
-        file.write(f"G92 Z0\n")
-        file.write(f"G0 Z{travel_height} F{seek}\n")
+        ws_send_and_get(ws, q, f"G38.2 Z{probe_depth} F{fine_seek}\n", data)
+        ws_send_and_get(ws, q, f"G92 Z0\n", data)
+        ws_send_and_get(ws, q, f"G0 Z{travel_height} F{seek}\n", data)
     
         for x in xv:
             for y in yv:
-                file.write(f"G1 X{x} Y{y} F{feed}\n")
-                file.write(f"G38.2 Z{probe_depth} F{seek}\n")
-                file.write(f"G0 Z2 F{seek}\n")
+                ws_send_and_get(ws, q, f"G1 X{x} Y{y} F{feed}\n", data)
+                ws_send_and_get(ws, q, f"G38.2 Z{probe_depth} F{seek}\n", data)
+                ws_send_and_get(ws, q, f"G0 Z2 F{seek}\n", data)
 
-        file.write(f"G0 Z{safe_height} F{seek}\n")
+        ws_send_and_get(ws, q, f"G0 Z{safe_height} F{seek}\n", data)
 
+        # signal to the thread to quit, it will see this at the 
+        # next ping from fluidnc
+        qdone.put(True);
+        t.join()
+
+        while not data.empty:
+            line = data.get()
+            print(line)
 
 if __name__ == '__main__':
     qt_mesh()
