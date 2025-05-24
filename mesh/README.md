547e4ce23b5425ccd8cdda3f8b2313700e6cfdf6
David Beck <beck.dac@live.com>
2025-05-24 11:50:22 -0700

    visualization of generated mesh

---
+++ mesh/README.md
@@ -0,0 +1,4 @@
+I had to use the following on WSL w/ 20.04
+```
+export QT_QPA_PLATFORM="xcb"
+```
--- mesh/qtdraw_mesh_plot.py
+++ mesh/qtdraw_mesh_plot.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
-import altair as alt
+import math
+
 import click
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
 
 def mesh_read(input_filename: str) -> pd.DataFrame:
     df = pd.read_csv(input_filename, sep='\t', usecols=('x','y','z'))
@@ -15,13 +20,42 @@ def mesh_read(input_filename: str) -> pd.DataFrame:
 @click.option('--input_filename', type=str, default='qtdraw_mesh.tsv')
 def qtdraw_mesh_plot(output_filename: str, input_filename: str):
     mesh = mesh_read(input_filename)
+    mesh = mesh.sort_values(by=['y', 'x'])
+    print(mesh)
 
-    chart = alt.Chart(mesh).mark_rect().encode(
-        x='x:O',
-        y='y:O',
-        color='z:Q'
-    )
-    chart.save(output_filename)
+    xv = mesh['x'].unique()
+    yv = mesh['y'].unique()
+    shape = (len(xv), len(yv))
+    x = mesh['x'].to_numpy().reshape(shape)
+    y = mesh['y'].to_numpy().reshape(shape)
+    z = mesh['z'].to_numpy().reshape(shape)
+
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    surf = ax.plot_surface(x, y, z, \
+        cmap=cm.jet, linewidth=1, rstride=1, cstride=1)
+    ax.set_zlim(z.min()-2, z.max()+2)
+    ax.zaxis.set_major_locator(LinearLocator(11))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    ax.invert_xaxis()
+    fig.colorbar(surf, shrink=0.3, aspect=5, pad=0.18)
+
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    surf = ax.plot_surface(x, y, z, \
+        cmap=cm.jet, linewidth=1, rstride=1, cstride=1)
+    ax.zaxis.set_major_locator(LinearLocator(11))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    ax.invert_xaxis()
+    fig.colorbar(surf, shrink=0.3, aspect=5, pad=0.18)
+
+    plt.show()
+    print(f"saving to '{output_filename}'")
+    plt.savefig(output_filename)
 
 if __name__ == '__main__':
     qtdraw_mesh_plot()
