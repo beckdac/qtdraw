adb72dbeceb8d4a2f6a225e32ae28ae9e8d2f471
David Beck <beck.dac@live.com>
2025-05-23 22:13:00 -0700

    working websocket cat basically

--- mesh/qtdraw_mesh.py
+++ mesh/qtdraw_mesh.py
@@ -6,25 +6,33 @@ import threading
 import websocket
 
 ws = websocket.WebSocket()
-ws.connect("ws://qtdraw.local:81")
+ws.connect('ws://qtdraw.local:81')
 
 # Reception needs to be done in a separate thread; you cannot
 # assume that a given command will always result in exactly one
 # response at a predictable time
 def receiver():
     while True:
-        for l in ws.recv().splitlines():
-            if isinstance(l, str):
-                print(l)
+        for line in ws.recv().splitlines():
+            if not isinstance(line, str):
+                line = str(line, 'utf-8')
+            if line.startswith("PING:") or \
+                  line.startswith("CURRENT_ID:") or \
+                  line.startswith("ACTIVE_ID:"):
+                continue
             else:
-                print(str(l, 'utf-8'))
+                print(line)
+
+def sender():
+    #from testing
+    #ws.send('?')  # realtime characters need no line terminator
+    #ws.send('$/axes/x\n')  # line-oriented commands need \n at the end
+    for line in sys.stdin:
+        ws.send(line)
+
 
 t = threading.Thread(target=receiver)
 t.start()
-
-def sender():
-    ws.send("?")  # realtime characters need no line terminator
-    ws.send("$/axes/x\n")  # line-oriented commands need \n at the end
 
 
 if __name__ == '__main__':
