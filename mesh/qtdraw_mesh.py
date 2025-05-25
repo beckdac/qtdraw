56c60946790a08e681bf1e06f5e3dbb3adcd2151
David Beck <beck.dac@live.com>
2025-05-25 13:31:14 -0700

    removed websocket sender and divided into work modes

--- mesh/qtdraw_mesh.py
+++ mesh/qtdraw_mesh.py
@@ -1,70 +1,23 @@
 #!/usr/bin/env python
 
+import enum
 import sys
-import queue
 import threading
 import typing
 
-from alive_progress import alive_bar
 import click
 import numpy as np
 import pandas as pd
-import websocket
 
-# websocket portion
-def receiver(ws: websocket.WebSocket, q: queue.Queue, qdone: queue.Queue):
-    """Receive websocket messages and swallow pings and other session info
-    placing the input lines in a queue for the main thread."""
-    while True:
-        for line in ws.recv().splitlines():
-            if not isinstance(line, str):
-                line = str(line, 'utf-8')
-            if line.startswith("<Alarm"):
-                print(line)
-                raise Exception("Alarm! received from host.")
-            if line.startswith("PING:") or \
-                  line.startswith("CURRENT_ID:") or \
-                  line.startswith("ACTIVE_ID:") or \
-                  line.startswith("<Run|WPos") or \
-                  line.startswith("<Idle|WPos") or \
-                  line.startswith("<Run|MPos") or \
-                  line.startswith("<Idle|MPos"):
-                if not qdone.empty():
-                    return
-                continue
-            else:
-                #print(line)
-                q.put(line)
-            if not qdone.empty():
-                return
-
-def ws_send_and_get(ws: websocket.WebSocket, q: queue.Queue, send: str, \
-        data: queue.Queue, log: typing.TextIO, gcode: typing.TextIO, \
-        expect: str="ok") -> str:
-    """This is the main send and get result function that compares against
-    an expected 'ok' style result. Any lines not matching the expected 
-    result are assumed to be current or previous (pending in queue) probe
-    responses from fluidnc.  They are added to a queue to be dequeued when
-    probing is complete for later parsing. To make it easier to track
-    what is happening, the unexpected responses are also written to a log
-    file passed in.
-    """
-    ws.send(send);
-    gcode.write(send)
-    gcode.flush()
-    while True:
-        resp = q.get()
-        if resp.startswith(expect):
-            #print(f"expected response to {send} : {resp}")
-            break
-        #print(f"unexpected response to {send} : {resp}")
-        data.put(resp)
-        log.write(resp)
-        log.flush() # force write for logging purposes
-    return resp
+class WorkMode(enum.Enum):
+    gcode = enum.auto()
+    parse = enum.auto()
 
 @click.command()
-@click.option('--lim', nargs=2, type=int, default=(195, 235), \
+@click.option('--work-mode', default=WorkMode.gcode, \
+              type=click.Choice(WorkMode, case_sensitive=False),
+              help='Choose tool function from list')
+@click.option('--lim', nargs=2, type=int, default=(200, 240), \
         help='Size limit of the work area in mm')
 @click.option('--div', nargs=2, type=int, default=(10, 11), \
         help='Samples per axis')
@@ -78,97 +31,91 @@ def ws_send_and_get(ws: websocket.WebSocket, q: queue.Queue, send: str, \
         help='Travel height for Z from sample to sample')
 @click.option('--safe_height', type=int, default=15, \
         help='Safe height after meshing')
-@click.option('--uri', type=str, default='ws://qtdraw.local:81', \
-        help='Output websocket to use for gcode input and output')
-@click.option('--output_filename', type=str, default='qtdraw_mesh.tsv')
-@click.option('--log_filename', type=str, default='qtdraw_mesh.log')
-@click.option('--gcode_filename', type=str, default='qtdraw_mesh.gcode')
-@click.option('--probe_x_offset', type=int, default=26)
-@click.option('--probe_y_offset', type=int, default=29)
+@click.option('--output_mesh_filename', type=str, default='qtdraw_mesh.tsv')
+@click.option('--input_log_filename', type=str, default='qtdraw_mesh.log')
+@click.option('--output_gcode_filename', type=str, default='qtdraw_mesh.gcode')
+@click.option('--probe_x_offset', type=int, default=22)
+@click.option('--probe_y_offset', type=int, default=27)
+#@click.option('', type=int, default=)
 def qt_mesh(lim, div, \
         feed, seek, probe_depth, travel_height, safe_height, \
-        output_filename, log_filename, gcode_filename, uri, \
-        probe_x_offset, probe_y_offset):
-    """Generate G-code for fluidnc and parse the output to build
-    a height map of the bed by communicating over a websocket and
-    parsing the responses for probe messages."""
+        output_mesh_filename, input_log_filename, output_gcode_filename, \
+        probe_x_offset, probe_y_offset, work_mode: WorkMode):
+    """Do one of several different tasks related to mapping a bed
+    and remapping G-code (gcode, GCODE, whatever) in the Z axis.
 
-    ws = websocket.WebSocket()
-    ws.connect(uri)
-    q = queue.Queue()
-    qdone = queue.Queue()
-    data = queue.Queue()
-    t = threading.Thread(target=receiver, args=(ws,q,qdone))
-    t.start()
+    gcode mode:
+    Generate G-code for fluidnc. Should work with a lot of grbl.
 
-    xv = np.linspace(0, lim[0], div[0])
-    yv = np.linspace(0, lim[1], div[1])
+    parse:
+    Parse the output to a mesh file from std in or a file. Assumes
+    global position in PRB as the custom from fluidnc. Outputs a 
+    table that can be used for down stream operations or in spread
+    sheet stuff or whatever.
 
-    print(f"generating {div[0] * div[1]} points on a grid from ({xv[0]},{yv[0]}) to ({xv[-1]},{yv[-1]})")
+    remap:
+    Take a map from a tsv and a gcode file and remap the z axis
+    onto the bed mesh using a very simple approach that only uses
+    lines. NO ARCS.  Yet, but that is a goal.
+    """
 
-    with alive_bar(len(xv) * len(yv)) as bar, \
-            open(log_filename, 'w') as log, \
-            open(gcode_filename, 'w') as gcode:
-        # preamble
-        # we do this in machine coordinates
-        ws_send_and_get(ws, q, f"G53 G90 G21 G17\n", data, log, gcode)
-        fine_seek = seek / 2
-        # travel to safe height
-        ws_send_and_get(ws, q, f"G0 Z{safe_height} F{seek}\n", data, log, gcode)
+    if (work_mode == WorkMode.gcode):
+        xv = np.linspace(0, lim[0], div[0])
+        yv = np.linspace(0, lim[1], div[1])
+
+        print(f"generating {div[0] * div[1]} points on a grid from ({xv[0]},{yv[0]}) to ({xv[-1]},{yv[-1]}) and saving to '{output_gcode_filename}'")
+
+        with open(output_gcode_filename, 'w') as gcode:
+            # preamble
+            # we do this in machine coordinates
+            gcode.write(f"G90 G21 G17\n")
+            fine_seek = seek / 2
+            # travel to safe height
+            gcode.write(f"G0 Z{safe_height} F{seek}\n")
 
 
-        def ws_send_xy(x, y):
-            ws_send_and_get(ws, q, f"G1 X{x:0.4f} Y{y:0.4f} F{feed}\n", data, log, gcode)
-            ws_send_and_get(ws, q, f"G38.2 Z{probe_depth} F{fine_seek}\n", data, log, gcode)
-            ws_send_and_get(ws, q, f"G0 Z2 F{seek}\n", data, log, gcode)
-            bar()
+            def write_probe_at_xy(x, y):
+                gcode.write(f"G1 X{x:0.4f} Y{y:0.4f} F{feed}\n")
+                gcode.write(f"G38.2 Z{probe_depth} F{fine_seek}\n")
+                gcode.write(f"G0 Z2 F{seek}\n")
 
-        # traverse the matrix but zig zag
-        for i, x in enumerate(xv):
-            if i % 2 == 0:
-                for y in yv:
-                    ws_send_xy(x, y)
-            else:
-                for y in reversed(yv):
-                    ws_send_xy(x, y)
+            # traverse the matrix but zig zag
+            for i, x in enumerate(xv):
+                if i % 2 == 0:
+                    for y in yv:
+                        write_probe_at_xy(x, y)
+                else:
+                    for y in reversed(yv):
+                        write_probe_at_xy(x, y)
 
-        ws_send_and_get(ws, q, f"G0 Z{safe_height} F{seek}\n", data, log, gcode)
-        ws_send_and_get(ws, q, f"G54\n", data, log, gcode)
-        
+            gcode.write(f"G0 Z{safe_height} F{seek}\n")
+    elif (work_mode == WorkMode.parse):
+        print(f"parsing mesh data from log in '{input_log_filename}'")
+        mesh_data = []
+        with open(input_log_filename, 'r') as log:
+          for line in log.readlines():
+            line = line.strip()
+            #print(line)
+            if not line.startswith("[PRB:"):
+                #raise ValueError(f"unable to find probe prefix '[PRB:' at start of data: {line}")
+                print(f"ignoring line that doesn't look like a PRB line: {line}")
+            colon_fields = line.split(":")
+            if len(colon_fields) != 3:
+                raise ValueError(f"expected 3 fields in probe data after split on ':', found {len(colon_fields)}, in data: {line}")
+            xyz = colon_fields[1].split(",")
+            if len(xyz) != 3:
+                raise ValueError(f"expected 3 fields (x, y, z) in second : split field when splitting on ',', found {len(xyz)} in data: {line}")
+            if colon_fields[2] != "1]":
+                raise ValueError(f"expected to find true (1) value at probe success field in data, found: {colon_fields[2]} in data: {line}")
+            #print(f"x = {xyz[0]}, y = {xyz[1]}, z = {xyz[2]}")
+            mesh_data.append(xyz)
 
-    # signal to the thread to quit, it will see this at the 
-    # next ping from fluidnc
-    qdone.put(True);
+        df = pd.DataFrame(mesh_data, columns=("x", "y", "z"), dtype=float)
+        print(f"writing mesh x,y,z data to '{output_mesh_filename}")
+        df["x"] = df["x"] + probe_x_offset
+        df["y"] = df["y"] + probe_y_offset
+        df.to_csv(output_mesh_filename, sep='\t', header=True, index=False)
 
-    print(f"parsing mesh data")
-    mesh_data = []
-    while not data.empty():
-        line = data.get()
-        line = line.strip()
-        #print(line)
-        if not line.startswith("[PRB:"):
-            raise ValueError(f"unable to find probe prefix '[PRB:' at start of data: {line}")
-        colon_fields = line.split(":")
-        if len(colon_fields) != 3:
-            raise ValueError(f"expected 3 fields in probe data after split on ':', found {len(colon_fields)}, in data: {line}")
-        xyz = colon_fields[1].split(",")
-        if len(xyz) != 3:
-            raise ValueError(f"expected 3 fields (x, y, z) in second : split field when splitting on ',', found {len(xyz)} in data: {line}")
-        if colon_fields[2] != "1]":
-            raise ValueError(f"expected to find true (1) value at probe success field in data, found: {colon_fields[2]} in data: {line}")
-        #print(f"x = {xyz[0]}, y = {xyz[1]}, z = {xyz[2]}")
-        mesh_data.append(xyz)
-
-    df = pd.DataFrame(mesh_data, columns=("x", "y", "z"), dtype=float)
-    print(f"writing mesh x,y,z data to '{output_filename}")
-    df["x"] = df["x"] + probe_x_offset
-    df["y"] = df["y"] + probe_y_offset
-    df.to_csv(output_filename, sep='\t', header=True, index=False)
-
-    # do this absolutely last because the qdone put won't trigger a
-    # the receiver thread to leave until it gets a ping message so
-    # this waits as long as possible from the put
-    t.join()
 
 if __name__ == '__main__':
     qt_mesh()
