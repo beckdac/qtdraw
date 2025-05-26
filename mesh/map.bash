f4e6da05da70e3d39f4855f96141864706c6561d
David Beck <beck.dac@live.com>
2025-05-26 00:41:04 -0700

    map remap and upload

--- mesh/map.bash
+++ mesh/map.bash
@@ -4,3 +4,9 @@
 ./qtdraw_remap_gcode.py --input_gcode_filename ../map/railway.gcode --output_gcode_filename ../map/railway.remapped.gcode
 ./qtdraw_remap_gcode.py --input_gcode_filename ../map/way.gcode --output_gcode_filename ../map/way.remapped.gcode
 ./qtdraw_remap_gcode.py --input_gcode_filename ../map/building.gcode --output_gcode_filename ../map/building.remapped.gcode
+
+curl -F upload=@../map/landuse.remapped.gcode http://qtdraw.local/upload
+curl -F upload=@../map/natural.remapped.gcode http://qtdraw.local/upload
+curl -F upload=@../map/railway.remapped.gcode http://qtdraw.local/upload
+curl -F upload=@../map/way.remapped.gcode http://qtdraw.local/upload
+curl -F upload=@../map/building.remapped.gcode http://qtdraw.local/upload
