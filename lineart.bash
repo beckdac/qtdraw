5b2296d51c325ec755c778c80cd75f08f32eaa30
David Beck <beck.dac@live.com>
2025-05-26 00:38:48 -0700

    upgrades for usability

--- lineart.bash
+++ lineart.bash
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-dim=7cm
+dim=16cm
+tol=1mm
+tol=0.4mm
+tol=0.1mm
+minlen=1mm
+minlen=0.5mm
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <input svg>"
+  exit 1
+fi
+
+insvg="$1"
+outsvg=`echo $insvg | sed "s/.svg/.out.svg/g"`
+outgcode=`echo $insvg | sed "s/.svg/.gcode/g"`
 
 echo line plotting
-vpype read lineart.svg \
+vpype read $insvg \
     scaleto $dim $dim \
     splitall \
-    linemerge --tolerance 0.1mm \
+    linemerge --tolerance $tol \
     linesort \
     reloop \
     linesimplify \
-    filter --min-length 0.5mm \
-    write --center --page-size ${dim}x$dim lineart.out.svg
+    filter --min-length $minlen \
+    write --center --page-size ${dim}x$dim $outsvg
 
 echo gcode rendering
 vpype --config qtdraw.toml \
-    read lineart.out.svg \
-    gwrite -p qtdraw lineart.gcode
+    read $outsvg \
+    gwrite -p qtdraw $outgcode
